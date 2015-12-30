e8a3d234d8cffab01d34d9104c5a002c36619ec8
Chip Senkbeil <rcsenkbe@us.ibm.com>
2015-12-30 14:08:13 -0600

    Added initial plugin

---
+++ build.sbt
@@ -0,0 +1,64 @@
+
+organization := "org.scala-debugger"
+
+name := "scala-debugger-tools-plugin"
+
+sbtPlugin := true
+
+version := "1.0.0-SNAPSHOT"
+
+licenses += (
+  "Apache-2.0",
+  url("https://www.apache.org/licenses/LICENSE-2.0.html")
+)
+
+homepage := Some(url("http://scala-debugger.org"))
+
+scalaVersion := "2.10.5"
+
+scalacOptions ++= Seq(
+  "-encoding", "UTF-8", "-target:jvm-1.6",
+  "-deprecation", "-unchecked", "-feature",
+  "-Xfatal-warnings"
+) ++ (CrossVersion.partialVersion(scalaVersion.value) match {
+  case Some((2, scalaMajor)) if scalaMajor == 10 => Seq("-Ywarn-all")
+  case _ => Nil
+})
+
+javacOptions ++= Seq(
+  "-source", "1.6", "-target", "1.6", "-Xlint:all", "-Werror",
+  "-Xlint:-options", "-Xlint:-path", "-Xlint:-processing"
+)
+
+scalacOptions in (Compile, doc) ++= Seq(
+  "-no-link-warnings" // Suppress problems with Scaladoc @throws links
+)
+
+// Properly handle Scaladoc mappings
+autoAPIMappings := true
+
+// Prevent publishing test artifacts
+publishArtifact in Test := false
+
+publishMavenStyle := true
+
+pomExtra :=
+  <scm>
+    <url>git@github.com:ensime/scala-debugger-tools-plugin.git</url>
+    <connection>scm:git:git@github.com:ensime/scala-debugger-tools-plugin.git</connection>
+  </scm>
+    <developers>
+      <developer>
+        <id>senkwich</id>
+        <name>Chip Senkbeil</name>
+        <url>http://www.chipsenkbeil.org</url>
+      </developer>
+    </developers>
+
+publishTo := {
+  val nexus = "https://oss.sonatype.org/"
+  if (isSnapshot.value)
+    Some("snapshots" at nexus + "content/repositories/snapshots")
+  else
+    Some("releases"  at nexus + "service/local/staging/deploy/maven2")
+}
---
+++ src/main/scala/org/scaladebugger/tools/plugin/ScalaDebuggerToolsPlugin.scala
@@ -0,0 +1,53 @@
+package org.scaladebugger.tools.plugin
+
+import java.io.FileNotFoundException
+
+import sbt._
+import sbt.Keys._
+
+import scala.util.Try
+
+/**
+ * Represents the main plugin to add tooling settings to projects using
+ * libraries from the Scala Debugger project.
+ */
+object ScalaDebuggerToolsPlugin extends AutoPlugin {
+  override def requires = plugins.JvmPlugin
+  override def trigger = allRequirements
+
+  override def projectSettings: Seq[Def.Setting[_]] =
+    inConfig(Compile)(settings) ++
+    inConfig(Runtime)(settings) ++
+    inConfig(Test)(settings)
+
+  lazy val settings = baseScalaDebuggerToolsSettings
+
+  lazy val baseScalaDebuggerToolsSettings: Seq[Def.Setting[_]] = Seq(
+    // JDK Dependency (just for sbt, must exist on classpath for execution,
+    // cannot be redistributed)
+    internalDependencyClasspath += { Attributed.blank(JavaTools) }
+  )
+
+  //
+  // NOTE: Taken from Ensime Server project (when under BSD 3-clause)
+  // https://github.com/ensime/ensime-server/blob/master/project/EnsimeBuild.scala
+  //
+  // WORKAROUND: https://github.com/typelevel/scala/issues/75
+  val JavaTools: File = List(
+    // manual
+    sys.env.get("JDK_HOME"),
+    sys.env.get("JAVA_HOME"),
+    // osx
+    Try("/usr/libexec/java_home".!!).toOption,
+    // fallback
+    sys.props.get("java.home").map(new File(_).getParent),
+    sys.props.get("java.home")
+  ).flatten.map { n =>
+    new File(n.trim + "/lib/tools.jar")
+  }.find(_.exists()).getOrElse(
+    throw new FileNotFoundException("""
+      |Could not automatically find the JDK/lib/tools.jar.
+      |You must explicitly set JDK_HOME or JAVA_HOME.
+    """.stripMargin)
+  )
+}
